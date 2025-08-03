fc346186ee6826802f01a08862308a9084e1d338
Sandeepa-git <agsvwimalasiri@gmail.com>
2025-08-03 09:51:27 +0530

    Initial commit

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://my-portfolio-18hv.onrender.com', // <-- Add your site URL here
+  siteUrl: 'https://my-portfolio-43so.vercel.app/', // <-- Add your site URL here
   generateRobotsTxt: true, // optional
   // other settings
 };
