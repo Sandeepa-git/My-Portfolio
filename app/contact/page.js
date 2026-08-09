afd7dd194b934bf652dad21a624b046cd64fe0fe
Sandeepa-git <agsvwimalasiri@gmail.com>
2026-08-09 12:41:08 +0530

    Remove Medium from contact section

--- app/contact/page.js
+++ app/contact/page.js
@@ -8,11 +8,6 @@ const WhatsAppIcon = ({ size }) => (
   </svg>
 );
 
-const MediumIcon = ({ size }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="currentColor">
-    <path d="M13.54 12a6.8 6.8 0 01-6.77 6.82A6.8 6.8 0 010 12a6.8 6.8 0 016.77-6.82A6.8 6.8 0 0113.54 12zM20.96 12c0 3.54-1.51 6.42-3.38 6.42-1.87 0-3.39-2.88-3.39-6.42s1.52-6.42 3.39-6.42 3.38 2.88 3.38 6.42M24 12c0 3.17-.53 5.75-1.19 5.75-.66 0-1.19-2.58-1.19-5.75s.53-5.75 1.19-5.75C23.47 6.25 24 8.83 24 12z"/>
-  </svg>
-);
 
 export default function Contact() {
   return (
@@ -62,17 +57,7 @@ export default function Contact() {
           </div>
         </div>
 
-        <div className="contact-item">
-          <div className="contact-icon" style={{ backgroundColor: 'transparent', color: 'var(--text-color)' }}>
-            <MediumIcon size={24} />
-          </div>
-          <div className="contact-details">
-            <h3>Medium</h3>
-            <a href="https://medium.com/@agsvwimalasiri" target="_blank" rel="noreferrer">
-              Read my articles
-            </a>
-          </div>
-        </div>
+
       </div>
     </div>
   );
